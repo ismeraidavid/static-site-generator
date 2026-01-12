96dcba597bdee17924e73ed9a2e774450ed5847b
ismeraidavid <ismeraidavidgr@gmail.com>
2026-01-12 12:49:29 +0000

    Refactor TextType enum and enhance text_node_to_html_node function for improved clarity and consistency

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode, ParentNode
 
 
 class TextType(Enum):
@@ -6,8 +7,8 @@ class TextType(Enum):
     BOLD_TEXT = "bold_text"
     ITALIC_TEXT = "italic_text"
     CODE_TEXT = "code_text"
-    LINKS = "links"
-    IMAGES = "images"
+    LINK = "link"
+    IMAGE = "image"
 
 
 class TextNode:
@@ -27,3 +28,28 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode):
+    if text_node.text_type not in TextType:
+        raise AttributeError("Invalid text type for the text node")
+
+    if text_node.text_type == TextType.BOLD_TEXT:
+        return LeafNode("b", text_node.text, None)
+
+    elif text_node.text_type == TextType.CODE_TEXT:
+        return LeafNode("code", text_node.text, None)
+
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode(
+            "img", value="", props={"src": text_node.url, "alt": text_node.text}
+        )
+
+    elif text_node.text_type == TextType.ITALIC_TEXT:
+        return LeafNode("i", text_node.text, None)
+
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, props={"href": text_node.url})
+
+    else:
+        return LeafNode(tag=None, value=text_node.text)
